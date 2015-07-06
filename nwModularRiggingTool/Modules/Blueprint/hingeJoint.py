a6c4b7b74672772a39daa2ce9dda4f103e5dd3f8
Tengil <niklas.wennersten@hotmail.com>
2015-07-06 12:36:03 +0200

    implemented mirror and lock functionality for hinge joint module

--- nwModularRiggingTool/Modules/Blueprint/hingeJoint.py
+++ nwModularRiggingTool/Modules/Blueprint/hingeJoint.py
@@ -39,6 +39,45 @@ class HingeJoint(blueprint.Blueprint):
 				
 				index += 1
 		
+		else:
+			rootJointName = self.jointInfo[0][0]
+			tempDuplicateNodes = pm.duplicate("%s:IK_%s" %(self.originalModule, rootJointName), renameChildren = True)
+			
+			# Make sure entire hierarchy is being stored in tempDuplicateNodes list
+			pm.select(tempDuplicateNodes[0], hierarchy = True)
+			tempDuplicateNodes = pm.ls(selection = True)
+			
+			
+			pm.delete(tempDuplicateNodes.pop())
+			
+			mirrorXY = False
+			mirrorYZ = False
+			mirrorXZ = False
+			
+			if self.mirrorPlane == "XY":
+				mirrorXY = True
+			elif self.mirrorPlane == "YZ":
+				mirrorYZ = True
+			elif self.mirrorPlane == "XZ":
+				mirrorXZ = True
+			
+			mirrorBehavior = False
+			if self.rotationFunction == "behaviour":
+				mirrorBehavior = True
+			
+			
+			mirrorJoints = pm.mirrorJoint(tempDuplicateNodes[0], mirrorXY = mirrorXY, mirrorYZ = mirrorYZ, mirrorXZ = mirrorXZ, mirrorBehavior = mirrorBehavior)
+			
+			pm.delete(tempDuplicateNodes)
+			pm.xform(mirrorJoints[0], worldSpace = True, absolute = True, translation = pm.xform("%s:%s" %(self.moduleNamespace, rootJointName), query = True, worldSpace = True, translation = True))
+			
+			
+			for i in range(3):
+				jointName = self.jointInfo[i][0]
+				newName = pm.rename(mirrorJoints[i], "%s:IK_%s" %(self.moduleNamespace, jointName))
+				ikJoints.append(newName)
+		
+		
 		utils.AddNodeToContainer(self.containerName, ikJoints)
 		
 		
@@ -71,3 +110,69 @@ class HingeJoint(blueprint.Blueprint):
 		
 		
 		pm.setAttr("%s.axis" %preferredAngleRep, lock = True)
+	
+	
+	def UI_custom(self):
+		joints = self.GetJoints()
+		
+		self.CreateRotationOrderUIControl(joints[0])
+		self.CreateRotationOrderUIControl(joints[1])
+	
+	
+	def Lock_phase1(self):
+		
+		# Gather and return all require information from this module's control objects
+	
+		# jointPositions = List of joint position, from root down the hierarchy
+		# jointOrientations = list of orientations, or list of axis information (orientJoint and secondaryAxisOrient for joint command)
+		#               # These are passed in the following tuple: (orientation, None) or (None, axisInfo)
+	
+		# jointRotationOrder = list of joint rotation orders (integer values gathered with getAttr)
+		# jointPreferredAngles = list of joint preferred angles, optional (can pass None)
+		# hookObject = self.FindHookObjectForLock()
+		# rootTransform = bool, either true or false. True = rotate, translate and scale on root joint. False = rotate only
+	
+		# moduleInfo = (jointPositions, jointOrientations, jointRotationOrders, jointPreferredAngles, hookObject, rootTransform)
+		# return moduleInfo
+	
+		jointPositions = []
+		jointOrientationValues = []
+		jointRotationOrders = []
+		jointPreferredAngles = []
+		
+		pm.lockNode(self.containerName, lock = False, lockUnpublished = False)
+		
+		ikHandle = "%s:IK_%s_ikHandle" %(self.moduleNamespace, self.jointInfo[0][0])
+		pm.delete(ikHandle)
+		
+		for i in range(3):
+			jointName = self.jointInfo[i][0]
+			ikJointName = "%s:IK_%s" %(self.moduleNamespace, jointName)
+			
+			pm.makeIdentity(ikJointName, rotate = True, translate = False, scale = False, apply = True)
+			
+			jointPositions.append(pm.xform(ikJointName, query = True, worldSpace = True, translation = True))
+			
+			jointRotationOrders.append(pm.getAttr("%s:%s.rotateOrder" %(self.moduleNamespace, jointName)))
+			
+			if i < 2:
+				jointOrientX = pm.getAttr("%s.jointOrientX" %ikJointName)
+				jointOrientY = pm.getAttr("%s.jointOrientY" %ikJointName)
+				jointOrientZ = pm.getAttr("%s.jointOrientZ" %ikJointName)
+				
+				jointOrientationValues.append( (jointOrientX, jointOrientY, jointOrientZ) )
+				
+				joint_preferredAngle_X = pm.getAttr("%s.preferredAngleX" %ikJointName)
+				joint_preferredAngle_Y = pm.getAttr("%s.preferredAngleY" %ikJointName)
+				joint_preferredAngle_Z = pm.getAttr("%s.preferredAngleZ" %ikJointName)
+				
+				jointPreferredAngles.append( (joint_preferredAngle_X, joint_preferredAngle_Y, joint_preferredAngle_Z) )
+		
+		
+		jointOrientations = (jointOrientationValues, None)
+		
+		hookObject = self.FindHookObjectForLock()
+		rootTransform = False
+		
+		moduleInfo = (jointPositions, jointOrientations, jointRotationOrders, jointPreferredAngles, hookObject, rootTransform)
+		return moduleInfo
