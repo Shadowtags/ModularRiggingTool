d51026efe3c048545cbcb0c171f87cf74a596dd6
Tengil <niklas.wennersten@hotmail.com>
2015-07-23 13:05:05 +0200

    implemented base for animation control UI

---
+++ nwModularRiggingTool/Modules/System/animation_UI.py
@@ -0,0 +1,145 @@
+import pymel.core as pm
+import os
+import System.utils as utils
+import System.controlObject as controlObject
+from functools import partial
+
+reload(utils)
+reload(controlObject)
+
+
+
+class Animation_UI:
+	
+	def __init__(self):
+		self.directory = "%s/nwModularRiggingTool" %pm.internalVar(userScriptDir = True)
+		
+		self.previousBlueprintListEntry = None
+		self.previousBlueprintModule = None
+		self.prebiousAnimationModule = None
+		
+		#baseIconsDir = "%s/Icons/" %self.directory
+		baseIconsDir = "%s/Icons/" %os.environ["RIGGING_TOOL_ROOT"]
+		
+		self.selectedCharacter = self.FindSelectedCharacter()
+		
+		if self.selectedCharacter == None:
+			return
+		
+		self.characterName = self.selectedCharacter.partition("__")[2]
+		
+		self.windowName = "%s_window" %self.characterName
+		
+		# Create UI
+		self.UIElements = {}
+		
+		if pm.window(self.windowName, exists = True):
+			pm.deleteUI(self.windowName)
+		
+		self.windowWidth = 420
+		self.windowHeight = 730
+		
+		self.UIElements["window"] = pm.window(self.windowName, width = self.windowWidth, height = self.windowHeight, title = "Animation UI: %s" %self.characterName, sizeable = False)
+		
+		self.UIElements["topColumnLayout"] = pm.columnLayout(adjustableColumn = True, rowSpacing = 3, parent = self.UIElements["window"])
+		
+		buttonWidth = 32
+		columnOffset = 5
+		buttonColumnWidth = buttonWidth + (2 * columnOffset)
+		textScrollWidth = (self.windowWidth - buttonColumnWidth - 8) / 2
+		
+		self.UIElements["listboxRowLayout"] = pm.rowLayout(numberOfColumns = 3, columnWidth3 = [textScrollWidth, textScrollWidth, buttonColumnWidth], columnAttach = ([1, "both", columnOffset], [2, "both", columnOffset], [3, "both", columnOffset]), parent = self.UIElements["topColumnLayout"])
+		
+		self.UIElements["blueprintModule_textScroll"] = pm.textScrollList(numberOfRows = 12, allowMultiSelection = False, selectCommand = self.RefreshAnimationModuleList, parent = self.UIElements["listboxRowLayout"])
+		self.InitializeBlueprintModuleList()
+		
+		self.UIElements["animationModule_textScroll"] = pm.textScrollList(numberOfRows = 12, allowMultiSelection = False, selectCommand = self.RefreshAnimationModuleList, parent = self.UIElements["listboxRowLayout"])
+		
+		self.UIElements["buttonColumnLayout"] = pm.columnLayout(parent = self.UIElements["listboxRowLayout"])
+		self.UIElements["pinButton"] = pm.symbolCheckBox(onImage = "%s_pinned.xpm" %baseIconsDir, offImage = "%s_unpinned.xpm" %baseIconsDir, width = buttonWidth, height = buttonWidth, parent = self.UIElements["buttonColumnLayout"])
+		
+		if pm.objExists("%s:non_blueprint_grp" %self.selectedCharacter):
+			value = pm.getAttr("%s:non_blueprint_grp.display" %self.selectedCharacter)
+			self.UIElements["non_blueprintVisibility"] = pm.symbolCheckBox(image = "%s_shelf_character.xpm" %baseIconsDir, value = value, width = buttonWidth, height = buttonWidth, parent = self.UIElements["buttonColumnLayout"])
+		
+		value = pm.getAttr("%s:character_grp.animationControlVisibility" %self.selectedCharacter)
+		self.UIElements["animControlVisibility"] = pm.symbolCheckBox(image = "%s_visibility.xpm" %baseIconsDir, value = value, width = buttonWidth, height = buttonWidth, parent = self.UIElements["buttonColumnLayout"])
+		
+		self.UIElements["deleteModuleButton"] = pm.symbolButton(image = "%s_shelf_delete.xpm" %baseIconsDir, width = buttonWidth, height = buttonWidth, enable = False, parent = self.UIElements["buttonColumnLayout"])
+		self.UIElements["duplicateModuleButton"] = pm.symbolButton(image = "%s_duplicate.xpm" %baseIconsDir, width = buttonWidth, height = buttonWidth, enable = False, parent = self.UIElements["buttonColumnLayout"])
+		
+		
+		pm.separator(style = "in", parent = self.UIElements["topColumnLayout"])
+		
+		self.RefreshAnimationModuleList()
+		
+		
+		pm.showWindow(self.UIElements["window"])
+	
+	
+	def InitializeBlueprintModuleList(self):
+		pm.namespace(setNamespace = self.selectedCharacter)
+		blueprintNamespaces = pm.namespaceInfo(listOnlyNamespaces = True)
+		pm.namespace(setNamespace = ":")
+		
+		self.blueprintModules = {}
+		
+		if len(blueprintNamespaces) > 0:
+			for namespace in blueprintNamespaces:
+				blueprintModule = utils.StripLeadingNamespace(namespace)[1]
+				userSpecifiedName = blueprintModule.partition("__")[2]
+				
+				pm.textScrollList(self.UIElements["blueprintModule_textScroll"], edit = True, append = userSpecifiedName)
+				self.blueprintModules[userSpecifiedName] = namespace
+		
+		pm.textScrollList(self.UIElements["blueprintModule_textScroll"], edit = True, selectIndexedItem = 1)
+		
+		selectedBlueprintModule = pm.textScrollList(self.UIElements["blueprintModule_textScroll"], query = True, selectItem = True)
+		self.selectedBlueprintModule = self.blueprintModules[selectedBlueprintModule[0]]
+		
+		print self.selectedBlueprintModule
+	
+	
+	def RefreshAnimationModuleList(self, _index = 1):
+		pm.textScrollList(self.UIElements["animationModule_textScroll"], edit = True, removeAll = True)
+		
+		pm.symbolButton(self.UIElements["deleteModuleButton"], edit = True, enable = False)
+		pm.symbolButton(self.UIElements["duplicateModuleButton"], edit = True, enable = False)
+		
+		selectedBlueprintModule = pm.textScrollList(self.UIElements["blueprintModule_textScroll"], query = True, selectItem = True)
+		self.selectedBlueprintModule = self.blueprintModules[selectedBlueprintModule[0]]
+		
+		pm.namespace(setNamespace = self.selectedBlueprintModule)
+		controlModuleNamespaces = pm.namespaceInfo(listOnlyNamespaces = True)
+		pm.namespace(setNamespace = ":")
+		
+		if len(controlModuleNamespaces) != 0:
+			for module in controlModuleNamespaces:
+				moduleName = utils.StripAllNamespaces(module)[1]
+				pm.textScrollList(self.UIElements["animationModule_textScroll"], edit = True, append = moduleName)
+		
+			pm.textScrollList(self.UIElements["animationModule_textScroll"], edit = True, selectIndexedItem = _index)
+			
+			pm.symbolButton(self.UIElements["deleteModuleButton"], edit = True, enable = True)
+			pm.symbolButton(self.UIElements["duplicateModuleButton"], edit = True, enable = True)
+		
+		self.previousBlueprintListEntry = selectedBlueprintModule
+	
+	
+	
+	def FindSelectedCharacter(self):
+		
+		selection = pm.ls(selection = True, transforms = True)
+		character = None
+		
+		if len(selection) > 0:
+			selected = selection[0]
+			selectedNamespaceInfo = utils.StripLeadingNamespace(selected)
+			
+			if selectedNamespaceInfo != None:
+				selectedNamespace = selectedNamespaceInfo[0]
+				
+				if selectedNamespace.find("Character__") == 0:
+					character = selectedNamespace
+		
+		return character
